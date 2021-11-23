e56b32891af1732e7874e5aca12dbc66c5243a80
Martin Skopp <skopp@riege.com>
2021-11-23 17:11:09 +0100

    Issue #3: Fixing NPE when XFWB comes without SpecifiedLogisticsTransportMovement.StageCode.ModeCode value

--- src/test/java/com/riege/onerecord/converter/XFWB3toOneRecordConverterTest.java
+++ src/test/java/com/riege/onerecord/converter/XFWB3toOneRecordConverterTest.java
@@ -61,4 +61,48 @@ public class XFWB3toOneRecordConverterTest {
         Assertions.assertTrue(json.contains("Piece#goodsDescription\" : \"CONSOLIDATION\\nAS PER ATTACHED\\nMANIFEST\\nSECURE CARGO\\nNOT RESTRICTED\\nAIRLINE PHARMA\\nSERVICE\""));
     }
 
+    @Test
+    public void test88811111111noModeCode() throws JAXBException, JsonProcessingException {
+        String filename = "888-11111111_XFWB_noModeCode.xml";
+
+        InputStream is;
+        WaybillType xfwb;
+        XFWB3toOneRecordConverter converter;
+        String awb;
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        // mapper.disable(SerializationFeature.WRAP_ROOT_VALUE);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+
+        is = ClassLoader.getSystemResourceAsStream(filename);
+        xfwb = new ConverterUtil().unmarshalXFWB3(is);
+        awb = xfwb.getBusinessHeaderDocument().getID().getValue();
+        converter = new XFWB3toOneRecordConverter(xfwb);
+
+        Assertions.assertNotNull(converter.getValidationHints());
+        Assertions.assertFalse(converter.getValidationHints().isEmpty());
+        for (ValidationMessage msg : converter.getValidationHints()) {
+            System.out.println(awb + " HINT: " + msg.getMessage());
+        }
+
+        Assertions.assertNotNull(converter.getValidationWarnings());
+        Assertions.assertFalse(converter.getValidationWarnings().isEmpty());
+        for (ValidationMessage msg : converter.getValidationWarnings()) {
+            System.out.println(awb + " WARNING: " + msg.getMessage());
+        }
+
+        Assertions.assertNotNull(converter.getValidationErrors());
+        Assertions.assertTrue(converter.getValidationErrors().isEmpty());
+        for (ValidationMessage msg : converter.getValidationErrors()) {
+            System.out.println(awb + " ERROR: " + msg.getMessage());
+        }
+
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(converter.getOneRecordResult());
+        System.out.println(awb + " JSON=\n" + json);
+        Assertions.assertTrue(json.contains("customsInfoNote\" : \"USCI1234567812345678X7\""));
+        Assertions.assertTrue(json.contains("Piece#goodsDescription\" : \"CONSOLIDATION\\nAS PER ATTACHED\\nMANIFEST\\nSECURE CARGO\\nNOT RESTRICTED\\nAIRLINE PHARMA\\nSERVICE\""));
+    }
+
 }
