ef8382dff760f32a83ea2318fd5d5c06ca4bdce4
Kevin Schmengler <schmengler@riege.com>
2025-08-22 11:25:59 +0200

    Use CodeListElement for locationCodes

--- src/main/java/com/riege/onerecord/converter/XFZB3toOneRecordConverter.java
+++ src/main/java/com/riege/onerecord/converter/XFZB3toOneRecordConverter.java
@@ -163,13 +163,16 @@ public class XFZB3toOneRecordConverter extends CargoXMLtoOneRecordConverter<Wayb
             AuthenticationLocationType location = sigCarrier.getIssueAuthenticationLocation();
             if (location != null) {
                 waybill.setCarrierDeclarationPlace(ONERecordCargoUtil.create(Location.class));
-                waybill.getCarrierDeclarationPlace().setCode(value(location.getName()));
+                Set<CodeListElement> locationCodes = ONERecordCargoUtil.buildSet();
+                CodeListElement locationCode = createCodeListElementGeneral(value(location.getName()));
+                locationCodes.add(locationCode);
+                waybill.getCarrierDeclarationPlace().setLocationCodes(locationCodes);
             }
         }
         ConsignorAuthenticationType sigConsignor = xmlBH.getSignatoryConsignorAuthentication();
         if (sigConsignor != null) {
             String singleEntry = value(sigConsignor.getSignatory());
-            waybill.setConsignorDeclarationSignature(ONERecordCargoUtil.buildSet(singleEntry));
+            waybill.setConsignorDeclarationSignature(singleEntry);
         }
     }
 
